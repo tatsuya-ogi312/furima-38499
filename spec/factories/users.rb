86d3427dc6dc005777215c183fc4aa672cef36c4
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-09-28 16:58:35 +0900

    ユーザー管理機能修正３

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     japanese_user = Gimei.name
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'1a'+Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
     first_name            {japanese_user.first.kanji}
     last_name             {japanese_user.last.kanji}
