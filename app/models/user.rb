2ea12058d5d3795da4ba2ad262216358dbfa491a
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-09-28 16:30:40 +0900

    ユーザー管理機能修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          KANGI_RENGE = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
          KATAKANA_RENGE = /\A[ァ-ヶー－]+\z/.freeze
-  validates :password, format: { with: PASSWORD_REGEX }, allow_blank: true
+  validates :password, format: { with: PASSWORD_REGEX }
   validates :nickname, presence: true
   validates :last_name, presence: true
-  validates :last_name, format: { with: KANGI_RENGE }, allow_blank: true
+  validates :last_name, format: { with: KANGI_RENGE }
   validates :first_name, presence: true
-  validates :first_name, format: { with: KANGI_RENGE }, allow_blank: true
+  validates :first_name, format: { with: KANGI_RENGE }
   validates :last_name_kana, presence: true
-  validates :last_name_kana, format: { with: KATAKANA_RENGE }, allow_blank: true
+  validates :last_name_kana, format: { with: KATAKANA_RENGE }
   validates :first_name_kana, presence: true
-  validates :first_name_kana,format: { with: KATAKANA_RENGE }, allow_blank: true
+  validates :first_name_kana,format: { with: KATAKANA_RENGE }
   validates :birthday, presence: true
 
 end
