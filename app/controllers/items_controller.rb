ee67e4e4895bc5f225a6f8a166e7e6f1e1571b4e
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-09-30 20:39:40 +0900

    商品情報編集機能修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :set_item, except: [:index, :new, :create]
   before_action :contributor_confirmation, only: [:edit, :update]
 
